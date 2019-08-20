a92e69a4695d55fe764b20bb312acdab6b34c88c
arbhassan <hassanjawad1321@gmail.com>
2019-08-20 06:00:55 +0500

    Added ComponentDidMount to App Component and Also Added Three new components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
 import React from 'react';
+import firebase from 'firebase';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    selectedNoteIndex: null,
+    selectedNote: null,
+    notes: null,
+  };
+
   render() {
     return <div>Hello World</div>;
   }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data['id'] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({notes: notes});
+      });
+  };
 }
 
 export default App;
